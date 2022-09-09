10f407a28685f99129b90f124883ffa2b22039b6
Naftali-B <nbudamba@gmail.com>
2022-09-09 12:30:13 -0700

    Variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - prints unique  possible combinations of two two-digits
-
  * Return: alway 0
-
  */
 
 int main(void)
@@ -26,55 +21,55 @@ int main(void)
 
 	{
 
-	for (d2 = 0; d2 < 10; d2++)
+		for (d2 = 0; d2 < 10; d2++)
 
-	{
+		{
 
-	for (d3 = d1; d3 < 10; d3++)
+			for (d3 = d1; d3 < 10; d3++)
 
-	{
+			{
 
-	for (d4 = 0 ; d4 < 10; d4++)
+				for (d4 = 0 ; d4 < 10; d4++)
 
-	{
+				{
 
-	e1 = d1 * 10 + d2;
+					e1 = d1 * 10 + d2;
 
-	e2 = d3 * 10 + d4;
+					e2 = d3 * 10 + d4;
+
+					if (e2 > e1)
+
+					{
+
+					putchar(d1 + '0');
+
+					putchar(d2 + '0');
+
+					putchar(' ');
+
+					putchar(d3 + '0');
+
+					putchar(d4 + '0');
 
 
 
-	if (e2 > e1)
+					if (!((e1 == 98) && (e2 == 99)))
 
-	{
+					{
 
-	putchar(d1 + '0');
+					putchar(',');
 
-	putchar(d2 + '0');
+					putchar(' ');
 
-	putchar(' ');
+					}
 
-	putchar(d3 + '0');
+					}
 
-	putchar(d4 + '0');
+				}
 
-	if (!((e1 == 98) && (e2 == 99)))
+			}
 
-	{
-
-	putchar(',');
-
-	putchar(' ');
-
-	}
-
-	}
-
-	}
-
-	}
-
-	}
+		}
 
 	}
 
