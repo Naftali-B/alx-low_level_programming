29fbb99677d6b3b7b312e61aaca75512fbf17763
Naftali-B <nbudamba@gmail.com>
2022-09-09 12:15:43 -0700

    Variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,41 +1,60 @@
 #include <stdio.h>
 
 /**
- * main - Prints out all the numbers
- * between 0 - 1000 with non repeating
- * digits.
- * Return: Always 0 (Success)
+ *main - prints unique  possible combinations of 3 digits in which 012, 120,
+ * 102, 021, 201, 210 are considered the same combination
+ *Return: alway 0
  */
 
 int main(void)
 
 {
 
-	int f, m, l;
+	int d1, d2, d3;
 
 
 
-	for (f = '0'; f <= '9'; f++)
+	for (d1 = 0; d1 < 10; d1++)
 
-		for (m = '0'; m <= '9'; m++)
+	{
 
-			for (l = '0'; l <= '9'; l++)
+		for (d2 = d1 + 1; d2 < 10; d2++)
+
+		{
+
+			for (d3 = d2 + 1; d3 < 10; d3++)
 
 			{
 
-				if ((f < m) && (m < l))
+				putchar(d1 + '0');
 
-					putchar(f), putchar(m), putchar(l);
+				putchar(d2 + '0');
 
-				if (f != 55 || m != 56 || l != 57)
+				putchar(d3 + '0');
 
-					putchar(','), putchar(' ');
+
+
+				if ((d1 * 100 + d2 * 10 + d3) < 789)
+
+				{
+
+					putchar(',');
+
+					putchar(' ');
+
+
+
+				}
 
 			}
 
+		}
 
+	}
 
 	putchar('\n');
+
+
 
 	return (0);
 
