eef317312f8573469e893e80ba115cd9abc3eabe
Naftali-B <nbudamba@gmail.com>
2022-09-09 11:46:54 -0700

    Variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out all the values
+ * 0-9 along with spaces and commas
+ * acting as seperators.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int l;
+
+
+
+	for (l = '0'; l <= '9'; l++)
+
+		putchar(l);
+
+		if (l < 57)
+
+		{
+
+			/* putchar(44); Space | putchar(32); Comma */
+
+			putchar(' '), putchar(',');
+
+		}
+
+
+
+	putchar ('\n');
+
+	return (0);
+
+}
