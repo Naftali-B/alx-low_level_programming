838fd0a02badbb65e241db08961deb4e56379cde
Naftali-B <nbudamba@gmail.com>
2022-09-09 12:11:22 -0700

    Variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out all the numbers
+ * between 0 - 1000 with non repeating
+ * digits.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int f, m, l;
+
+
+
+	for (f = '0'; f <= '9'; f++)
+
+		for (m = '0'; m <= '9'; m++)
+
+			for (l = '0'; l <= '9'; l++)
+
+			{
+
+				if ((f < m) && (m < l))
+
+					putchar(f), putchar(m), putchar(l);
+
+				if (f != 55 || m != 56 || l != 57)
+
+					putchar(','), putchar(' ');
+
+			}
+
+
+
+	putchar('\n');
+
+	return (0);
+
+}
