76fc3f533f3b101d6ffd785728640640248446dc
Naftali-B <nbudamba@gmail.com>
2022-09-14 14:42:48 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,21 +14,21 @@ void print_to_98(int n)
 
 	{
 
-		while (n >= 98)
+	while (n >= 98)
 
-		{
+	{
 
-			if (n == 98)
+	if (n == 98)
 
-				printf("%d\n", n);
+	printf("%d\n", n);
 
-			else
+	else
 
-				printf("%d, ", n);
+	printf("%d, ", n);
 
-			n--;
+	n--;
 
-		}
+	}
 
 	}
 
@@ -36,21 +36,21 @@ void print_to_98(int n)
 
 	{
 
-		while (n <= 98)
+	while (n <= 98)
 
-		{
+	{
 
-			if (n == 98)
+	if (n == 98)
 
-				printf("%d\n", n);
+	printf("%d\n", n);
 
-			else
+	else
 
-				printf("%d, ", n);
+	printf("%d, ", n);
 
-			n++;
+	n++;
 
-		}
+	}
 
 	}
 
