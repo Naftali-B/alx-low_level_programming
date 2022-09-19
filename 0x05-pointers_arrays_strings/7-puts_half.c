31e4324f44d016ed8c813514d7c9e487e83f3443
Naftali-B <nbudamba@gmail.com>
2022-09-19 09:56:35 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,53 +10,29 @@
 void puts_half(char *str)
 
 {
-
 	int i, mid;
-
 	int c = 0;
 
-
-
 	while (str[c] != '\0')
-
 	{
-
 		c++;
-
 	}
-
-
 
 	if (c % 2 == 0)
-
 	{
-
 		for (i = c / 2; i < c; i++)
-
 		{
-
 			_putchar(str[i]);
-
 		}
-
 	}
-
 	else
-
 	{
-
 		mid = (c - 1) / 2;
-
 		for (i = mid + 1; i < c; i++)
-
 		{
-
 			_putchar(str[i]);
-
 		}
-
 	}
-
 	_putchar('\n');
 
 }
