95aaf5f57306a6a11787104761eb729ce21b77ff
Naftali-B <nbudamba@gmail.com>
2022-09-19 09:26:51 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the codes
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+	char *str;
+
+
+
+	str = "01234567879";
+
+	puts_half(str);
+
+	return (0)
+
+}
