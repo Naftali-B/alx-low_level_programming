0b9e14b8ca563b4b10767c3521aa6eebf887d8a9
Naftali-B <nbudamba@gmail.com>
2022-09-22 05:10:21 -0700

    Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,24 +5,19 @@
 * @dest: to be concatenated to
 * @src: to be concatenated to dest
 * @n: number of bytes
-
 * Return: returns dest
 */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
 	int i, j;
-
 	i = 0;
 	j = 0;
-
 	while (*(dest + i) != '\0')
 	{
 		j++;
 		i++;
 	}
-
 	i = 0;
 	while (*(src + i) != *(src + n))
 	{
@@ -34,5 +29,4 @@ char *_strncat(char *dest, char *src, int n)
 	return (dest);
 
 }
-
 
