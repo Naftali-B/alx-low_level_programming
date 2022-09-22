c9b9c790c05419401de3bc0a7a8a9c7096965ed4
Naftali-B <nbudamba@gmail.com>
2022-09-22 06:09:10 -0700

    Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,28 +12,31 @@
 char *_strncat(char *dest, char *src, int n)
 
 {
+
 	int i, j;
+
 	i = 0;
 	j = 0;
 
 	while (*(dest + i) != '\0')
 
 	{
+
 		j++;
 		i++;
+
 	}
 
 	i = 0;
-
 	while (*(src + i) != *(src + n))
+
 	{
 		*(dest + j) = *(src + i);
 		i++;
 		j++;
 	}
-
 	*(dest + j) = '\0';
+
 	return (dest);
 
 }
-
