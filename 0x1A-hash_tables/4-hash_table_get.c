de2eb9afa79a1ccfacca17a845897996f5151785
Naftali-B <nbudamba@gmail.com>
2023-11-07 10:13:31 +0300

    hash

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,37 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieve the value associated with a key in a hash table.
+ * hash_table_get - Retrieve the value associated with
+ *                  a key in a hash table.
  * @ht: A pointer to the hash table.
- * @key: The key to look up the value for.
+ * @key: The key to get the value of.
  *
- * Return: If the key cannot be found or is invalid - NULL.
- *         Otherwise - the value associated with the key in ht.
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *node;
-    unsigned long int index;
+	hash_node_t *node;
+	unsigned long int index;
 
-    /* Check for invalid or missing parameters */
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-    /* Calculate the index for the key using the key_index function */
-    index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
-    if (index >= ht->size)
-        return (NULL);
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
 
-    /* Traverse the linked list at the calculated index */
-    node = ht->array[index];
-    while (node != NULL)
-    {
-        if (strcmp(node->key, key) == 0)
-            return (node->value); // Return the value if the key is found
-        node = node->next;
-    }
-
-    return (NULL); // Key not found, return NULL
+	return (NULL);
 }
-
